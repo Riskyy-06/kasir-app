c34bf0f9af2cad90b1204d1db9d634e99bf1ce69
Riskyy-06 <aliffianrizki6@gmail.com>
2025-04-25 08:56:59 +0700

    dashboard, transaksi id

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Transaksi;
+use App\Models\Kategori;
+use App\Models\Produk;
+use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -85,7 +85,7 @@ class TransaksiController extends Controller
      */
     public function destroy(Transaksi $transaksi)
     {
-        $transaksi = Transaksi::findOrFail($id);
+        $transaksi = Transaksi::findOrFail($transaksi->id);
         $transaksi->delete();
 
         return redirect()->route('transaksi.index')->with('success', 'Transaksi berhasil dihapus');
